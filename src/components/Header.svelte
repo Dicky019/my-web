226367a232c6f1e958eeca317de6afba366e3dc4
Dicky Darmawan <dicky93darmawan@gmail.com>
2022-09-03 04:21:19 +0800

    tampilan hom

---
+++ src/components/Header.svelte
@@ -0,0 +1,8 @@
+<header class="flex justify-between sm:my-5 my-3">
+	<a class="font-bold text-lg md:text-xl" href="/">Dicky</a>
+	<div class="flex sm:gap-x-8 gap-x-5">
+        <a class="font-bold text-lg md:text-xl" href="/">About Us</a>
+		<a class="font-bold text-lg md:text-xl" href="/">Portofolio</a>
+		<a class="font-bold text-lg md:text-xl" href="/">Resume</a>
+	</div>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="md:mx-8 mx-4">
+	<slot />
+</div>
