b46146405acdc64286c720e6b78f642c7ac8050a
Dicky Darmawan <dicky93darmawan@gmail.com>
2022-09-03 14:52:37 +0800

    Header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,7 @@
 <header class="flex justify-between sm:my-5 my-3">
 	<a class="font-bold text-lg md:text-xl" href="/">Dicky</a>
 	<div class="flex sm:gap-x-8 gap-x-5">
-        <a class="font-bold text-lg md:text-xl" href="/">About Us</a>
+        <a class="font-bold text-lg md:text-xl" href="/">About</a>
 		<a class="font-bold text-lg md:text-xl" href="/">Portofolio</a>
 		<a class="font-bold text-lg md:text-xl" href="/">Resume</a>
 	</div>
